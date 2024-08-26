16ce55c7dc4aba2f4e734824e1a9cef26f53f812
Aisha <aishasalimg@gmail.com>
2024-08-25 22:31:55 -0500

    altered UI of invitation tab

--- frontend/src/components/dashboard/InboxOverview.jsx
+++ frontend/src/components/dashboard/InboxOverview.jsx
@@ -1,59 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from '../reusable'; // Adjust import path as necessary
+import React, { useState } from 'react';
 import { Mail } from 'lucide-react';
 import { useUserContext } from '../../contexts/userDataHandler';
+import moment from 'moment'; 
+import { momentLocalizer } from 'react-big-calendar'; 
+
+// Initialize the moment localizer
+const localizer = momentLocalizer(moment);
+
 /**
- * Returns an inbox view component
- * @param {Array<Object>} invitations - List of invitation objects
- * @returns A div containing the Inbox component
+ * Returns an Invitations overview
+ * @param {Array<Object>} events - the events to be displayed/rendered
+ * @param {boolean} isRightBarOpen - if the dashboard's right bar is open
+ * @param {Function} setIsRightBarOpen - function to set the dashboard's right bar's open status
+ * @param {String} rightBarContent - String stating what the right bar should display
+ * @param {Function} setRightBarContent - function to set the right bar content string
+ * @returns A div containing the Invitation component
  */
 const InboxOverview = () => {
-  const { invitations, acceptInvitation} = useUserContext(); // Get acceptInvitation and fetchSharedCalendars from userDataHandler
-  console.log(invitations)
+  const { invitations, acceptInvitation } = useUserContext();
+  const [currentTab, setCurrentTab] = useState('current'); // Default tab
+
   const handleInvitationResponse = async (token, action) => {
     acceptInvitation(token, action);
-    //await fetchSharedCalendars(); // Refresh the list of shared calendars
-    // Update local state to remove the responded invitation
-    // setUpdatedInvitations(prev => prev.filter(inv => inv.token !== token));
   };
 
+  const renderInvitations = (filterFn) => {
+    const filteredInvitations = invitations.filter(filterFn);
+  
+    const formatDate = (dateString) => {
+      const date = new Date(dateString);
+      const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+      return date.toLocaleDateString(undefined, options);
+    };
+
+    return filteredInvitations.length === 0 ? (
+      <p className="text-center text-gray-500">No invitations available.</p>
+    ) : (
+      <ul className="space-y-2 mt-15 max-h-90 overflow-y-auto">
+        {filteredInvitations.map((invitation, index) => (
+          <li 
+            key={index} 
+            className="relative flex flex-col py-2 px-5 my-2 mx-10 font-small rounded-lg border hover:bg-gray-100 transition-colors duration-300">
+          
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex items-center justify-center leading-4 p-2">
+                  <Mail style={{ width: '24px', height: '24px' }} />
+                </div>
+                <span className="ml-4 text-base text-gray-700">
+                  {`${invitation.invited_by.username} invited you to join the ${invitation.calendar.title} calendar`}
+                </span>
+              </div>
+              {!invitation.accepted && !invitation.declined && (
+                <div className="flex space-x-3">
+                  <button 
+                    onClick={() => handleInvitationResponse(invitation.token, 'accept')} 
+                    className="bg-green-700 hover:bg-green-600 text-white font-semibold py-1.5 px-4 rounded-lg transition-colors duration-300">
+                    Accept
+                  </button>
+                  <button 
+                    onClick={() => handleInvitationResponse(invitation.token, 'decline')} 
+                    className="bg-red-600 hover:bg-red-500 text-white font-semibold py-1.5 px-4 rounded-lg transition-colors duration-300">
+                    Decline
+                  </button>
+                </div>
+              )}
+              {(invitation.accepted || invitation.declined) && (
+                <div className="flex space-x-2">
+                  <span 
+                    className={`ml-4 text-sm font-semibold 
+                    ${invitation.accepted ? 'text-green-600' : 'text-red-600'}`}>
+                    {invitation.accepted ? 'Accepted' : 'Declined'}
+                  </span>
+                </div>
+              )}
+            </div>
+
+          {/* Display the date on top */}
+          <div className="text-gray-500 text-sm">
+            {formatDate(invitation.created_at)}
+          </div>
+
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  
   return (
     <div className="h-full pt-5">
-      {invitations.length === 0 ? (
-        <p className="text-center text-gray-500">No invitations available.</p>
-      ) : (
-        <ul className="space-y-2">
-          {invitations.map((invitation, index) => (
-            <li key={index} className="relative flex flex-col py-2 px-3 my-1 font-medium rounded-md bg-white shadow-md">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <div className="flex items-center justify-center leading-4">
-                    <Mail style={{ width: '32px', height: '32px' }} />
-                  </div>
-                  <span className="ml-2 text-sm">{invitation.invited_by.username + " invited you to join the " + invitation.calendar.title + " calendar"}</span>
-                </div>
-                { !invitation.accepted && !invitation.declined && 
-                  (<div className="flex space-x-2">
-                    <Button onClick={() => handleInvitationResponse(invitation.token, 'accept')} className="bg-green-500 text-white">
-                      Accept
-                    </Button>
-                    <Button onClick={() => handleInvitationResponse(invitation.token, 'decline')} className="bg-red-500 text-white">
-                      Decline
-                    </Button>
-                  </div>
-                )}
-                {(invitation.accepted || invitation.declined) &&
-                  (<div className="flex space-x-2">
-                    <span className={`ml-2 text-sm ${invitation.accepted ? "text-green-700" : "text-red-700"}`}>
-                      {invitation.accepted ? "Accepted" : "Declined"}
-                    </span>
-                  </div>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+      <div className="flex justify-center mb-5">
+        <button
+          onClick={() => setCurrentTab('current')}
+          className={`btn px-6 py-2.5 text-sm border border-r-0 rounded-l-xl uppercase transition-colors
+            ${currentTab === 'current' ? 'bg-gray-100 font-bold text-black' : 'text-gray-400 hover:bg-gray-100 hover:font-bold'}`}>
+          Current
+        </button>
+        <button
+          onClick={() => setCurrentTab('sent')}
+          className={`btn px-6 py-2.5 text-sm border uppercase transition-colors
+            ${currentTab === 'sent' ? 'bg-gray-100 font-bold text-black' : 'text-gray-400 hover:bg-gray-100 hover:font-bold'}`}>
+          Sent
+        </button>
+        <button
+          onClick={() => setCurrentTab('history')}
+          className={`btn px-6 py-2.5 text-sm border border-l-0 rounded-r-xl uppercase transition-colors
+            ${currentTab === 'history' ? 'bg-gray-100 font-bold text-black' : 'text-gray-400 hover:bg-gray-100 hover:font-bold'}`}>
+          History
+        </button>
+      </div>
+
+      {currentTab === 'current' && renderInvitations(invitation => !invitation.accepted && !invitation.declined)}
+      {currentTab === 'sent' && renderInvitations(invitation => invitation.sent_by_user)}
+      {currentTab === 'history' && renderInvitations(invitation => invitation.accepted || invitation.declined)}
     </div>
   );
 };
