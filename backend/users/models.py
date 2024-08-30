68e3d60f69c2c56b861282fb25c83e6a52b7a752
EsterIso <EsterlinjPaulino@gmail.com>
2024-08-29 21:04:57 -0400

    model change

--- backend/users/models.py
+++ backend/users/models.py
@@ -29,7 +29,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
     email = models.EmailField(_('email address'), unique=True)
     username = models.CharField(_('username'), max_length=255)
-    profile_picture = models.ImageField(upload_to='profile_pics', default=settings.DEFAULT_PROFILE_PIC)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
